dfba20c088f373ab1e3b127c7616a7da402af38e
Mahdi Hasani <mahdi.hasani@urbansportsclub.com>
2025-01-11 04:33:55 +0100

    organizing the entry point

--- app/cli/main.py
+++ app/cli/main.py
@@ -2,40 +2,26 @@ from builders import Editor
 from models import Image
 from models.filters import (
     AdaptiveThreshold,
-    AveragingBlur,
     GaussianBlur,
-    MedianBlur,
     GrayScale,
-    Threshold,
-    Denoise,
-    Smooth,
-    Sharp,
 )
-from models.operations import (
-    Brightness,
-    Resize,
-    Rotate,
-    Flip,
-    Crop,
-)
-from models.draw import (
-    Rectangle,
-    Circle,
-    Line,
-)
+
+# from models.filters import AveragingBlur, MedianBlur, Threshold, Denoise, Smooth, Sharp9
+# from models.operations import Brightness, Resize, Rotate, Flip, Crop
+# from models.draw import Rectangle, Circle, Line
 
 
 def main():
     image = Image("./images/image_2.webp")
 
     e = Editor(image)
-    e.add_layer(
-        GrayScale(),
-        GaussianBlur(kernel_size=(3, 3)),
-        AdaptiveThreshold(255, 3, 5),
-    )
 
-    image = e.apply()
+    image = e.add_layer(
+        GrayScale(),
+        GaussianBlur((3, 3)),
+        AdaptiveThreshold(255, 3, 5),
+    ).apply()
+
     image.show()
     image.store()
 
